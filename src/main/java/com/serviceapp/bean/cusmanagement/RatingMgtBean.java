ae8410d8fad1347e26cac6952bd187d75814ac9f
Prathibha Sathyajith <prathibha4you@gmail.com>
2018-04-17 17:02:10 +0530

    init

--- src/main/java/com/serviceapp/bean/cusmanagement/RatingMgtBean.java
+++ src/main/java/com/serviceapp/bean/cusmanagement/RatingMgtBean.java
@@ -10,5 +10,153 @@ package com.serviceapp.bean.cusmanagement;
  * @author prathibha
  */
 public class RatingMgtBean {
+    private String basic;
+    private String bronze;
+    private String gold;
+    private String platinum;
+    private String silver;
+    
+    private String basic_price;
+    private String bronze_price;
+    private String gold_price;
+    private String platinum_price;
+    private String silver_price;
+    
+    private String basic_count;
+    private String bronze_count;
+    private String gold_count;
+    private String platinum_count;
+    private String silver_count;
+    
+    private long fullCount;
+
+    public String getBasic() {
+        return basic;
+    }
+
+    public void setBasic(String basic) {
+        this.basic = basic;
+    }
+
+    public String getBronze() {
+        return bronze;
+    }
+
+    public void setBronze(String bronze) {
+        this.bronze = bronze;
+    }
+
+    public String getGold() {
+        return gold;
+    }
+
+    public void setGold(String gold) {
+        this.gold = gold;
+    }
+
+    public String getPlatinum() {
+        return platinum;
+    }
+
+    public void setPlatinum(String platinum) {
+        this.platinum = platinum;
+    }
+
+    public String getSilver() {
+        return silver;
+    }
+
+    public void setSilver(String silver) {
+        this.silver = silver;
+    }
+
+    public String getBasic_price() {
+        return basic_price;
+    }
+
+    public void setBasic_price(String basic_price) {
+        this.basic_price = basic_price;
+    }
+
+    public String getBronze_price() {
+        return bronze_price;
+    }
+
+    public void setBronze_price(String bronze_price) {
+        this.bronze_price = bronze_price;
+    }
+
+    public String getGold_price() {
+        return gold_price;
+    }
+
+    public void setGold_price(String gold_price) {
+        this.gold_price = gold_price;
+    }
+
+    public String getPlatinum_price() {
+        return platinum_price;
+    }
+
+    public void setPlatinum_price(String platinum_price) {
+        this.platinum_price = platinum_price;
+    }
+
+    public String getSilver_price() {
+        return silver_price;
+    }
+
+    public void setSilver_price(String silver_price) {
+        this.silver_price = silver_price;
+    }
+
+    public String getBasic_count() {
+        return basic_count;
+    }
+
+    public void setBasic_count(String basic_count) {
+        this.basic_count = basic_count;
+    }
+
+    public String getBronze_count() {
+        return bronze_count;
+    }
+
+    public void setBronze_count(String bronze_count) {
+        this.bronze_count = bronze_count;
+    }
+
+    public String getGold_count() {
+        return gold_count;
+    }
+
+    public void setGold_count(String gold_count) {
+        this.gold_count = gold_count;
+    }
+
+    public String getPlatinum_count() {
+        return platinum_count;
+    }
+
+    public void setPlatinum_count(String platinum_count) {
+        this.platinum_count = platinum_count;
+    }
+
+    public String getSilver_count() {
+        return silver_count;
+    }
+
+    public void setSilver_count(String silver_count) {
+        this.silver_count = silver_count;
+    }
+
+    public long getFullCount() {
+        return fullCount;
+    }
+
+    public void setFullCount(long fullCount) {
+        this.fullCount = fullCount;
+    }
+    
     
 }
